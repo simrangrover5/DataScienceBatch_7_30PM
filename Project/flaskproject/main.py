eb1ce1f97b7ce290077382cb73f4ad74c6ea986b
simrangrover5 <simrangrover5@gmail.com>
2021-06-02 20:13:10 +0530

    project

--- Project/flaskproject/main.py
+++ Project/flaskproject/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():  
     return render_template("index.html")
 
+@app.route("/filterjob/")
+def filterjob():
+    return render_template("jobform.html")
+
 app.run(host="localhost", debug=True)
 
